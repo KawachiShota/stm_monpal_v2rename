606853a65d6636460de9eb4a495718590dfbbc31
KawachiShota <kawachi-s@nkc-j.co.jp>
2016-04-26 11:38:15 +0900

    Update cylinder_ctl.py

--- cylinder_ctl.py
+++ cylinder_ctl.py
@@ -13,17 +13,64 @@ class Cylinder_controller:
 	rod_p = cylinder_list[number]		#位置を選ぶ
 
 
-        p1 = pyb.Pin(pyb.Pin.board.PE11, mode=pyb.Pin.OUT_PP)
-        p2 = pyb.Pin(pyb.Pin.board.PE12, mode=pyb.Pin.OUT_PP)
-        p3 = pyb.Pin(pyb.Pin.board.PE13, mode=pyb.Pin.OUT_PP)
-        p4 = pyb.Pin(pyb.Pin.board.PE14, mode=pyb.Pin.OUT_PP)
-        p5 = pyb.Pin(pyb.Pin.board.PE15, mode=pyb.Pin.OUT_PP)
+        p1 = pyb.Pin(pyb.Pin.board.PE11, mode=pyb.Pin.OUT_PP)   #位置情報1
+        p2 = pyb.Pin(pyb.Pin.board.PE12, mode=pyb.Pin.OUT_PP)   #位置情報2
+        p3 = pyb.Pin(pyb.Pin.board.PE13, mode=pyb.Pin.OUT_PP)   #位置情報3
+        p4 = pyb.Pin(pyb.Pin.board.PE14, mode=pyb.Pin.OUT_PP)   #実行命令
+#        p5 = pyb.Pin(pyb.Pin.board.PE15, mode=pyb.Pin.OUT_PP)
 
-        if rod_p == 1:
-            p1.high()
+        if rod_p == 1:				#各場合の出力
+            p1.low()
             p2.low()
+            p3.high()
+            time.sleep(0.01)
+            p4.high()
+            time.sleep(0.01)
+            p4.low()
 
         elif rod_p == 2:
             p1.low()
+            p2.low()
+            p3.low()
+            time.sleep(0.01)
+            p4.high()
+            time.sleep(0.01)
+            p4.low()
+
+        elif rod_p == 3:
+            p1.high()
+            p2.low()
+            p3.low()
+            time.sleep(0.01)
+            p4.high()
+            time.sleep(0.01)
+            p4.low()
+
+        elif rod_p == 4:
+            p1.high()
             p2.high()
+            p3.low()
+            time.sleep(0.01)
+            p4.high()
+            time.sleep(0.01)
+            p4.low()
+
+        elif rod_p == 5:
+            p1.low()
+            p2.low()
+            p3.low()
+            time.sleep(0.01)
+            p4.high()
+            time.sleep(0.01)
+            p4.low()
+
+        elif rod_p == 6:
+            p1.low()
+            p2.high()
+            p3.low()
+            time.sleep(0.01)
+            p4.high()
+            time.sleep(0.01)
+            p4.low()
+
 
